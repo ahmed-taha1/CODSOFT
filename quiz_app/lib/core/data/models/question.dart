fa023e824ac0d54e1f222e3786f9713236e471ad
Ahmed Taha <101575320+ahmed-taha1@users.noreply.github.com>
2024-02-18 22:03:34 +0200

    init commit

---
+++ quiz_app/lib/core/data/models/question.dart
@@ -0,0 +1,10 @@
+class Question{
+  String questionText;
+  List<String> answersTxt;
+  int answerIndex;
+
+  Question(
+      {required this.questionText,
+      required this.answersTxt,
+      required this.answerIndex});
+}
